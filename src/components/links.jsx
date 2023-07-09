ab3c086a3636eccf0d4a38c05dd3da20ecddc6ba
caiomeirelles22 <dev.meirelles22@gmail.com>
2023-07-09 14:36:31 -0300

    atualizando links

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -6,21 +6,21 @@ export default function Links() {
     return (
         <div>
             <ul className="lista-de-links">
-                <button className='item-link'><Link to={'/games/shooter'}>Shooter</Link></button>
-                <button className='item-link'><Link to={'/games/mmoarpg'}>Mmorpg</Link></button>
-                <button className='item-link'><Link to={'/games/fighting'}>Fighting</Link></button>
-                <button className='item-link'><Link to={'/games/moba'}>Moba</Link></button>
-                <button className='item-link'><Link to={'/games/sports'}>Sports</Link></button>
-                <button className='item-link'><Link to={'/games/racing'}>Racing</Link></button>
-                <button className='item-link'><Link to={'/games/card-Games'}>CardGames</Link></button>
-                <button className='item-link'><Link to={'/games/battle-royale'}>BattleRoyale</Link></button>
-                <button className='item-link'><Link to={'/games/strategy'}>Strategy</Link></button>
-                <button className='item-link'><Link to={'/games/action-rpg'}>ActionRpg</Link></button>
-                <button className='item-link'><Link to={'/games'}>All Games </Link></button>
-                <button className='item-link'><Link to={'/games'}>Em breve !!</Link></button>
-                <button className='item-link'><Link to={'/games'}>Em breve !!</Link></button>
-                <button className='item-link'><Link to={'/games'}>Em breve !!</Link></button>
-
+                <a className='item-link'><Link to={'/games/shooter'}>Shooter</Link></a>
+                <a className='item-link'><Link to={'/games/mmoarpg'}>Mmorpg</Link></a>
+                <a className='item-link'><Link to={'/games/fighting'}>Fighting</Link></a>
+                <a className='item-link'><Link to={'/games/moba'}>Moba</Link></a>
+                <a className='item-link'><Link to={'/games/sports'}>Sports</Link></a>
+                <a className='item-link'><Link to={'/games/racing'}>Racing</Link></a>
+                <a className='item-link'><Link to={'/games/card-Games'}>CardGames</Link></a>
+                <a className='item-link'><Link to={'/games/battle-royale'}>BattleRoyale</Link></a>
+                <a className='item-link'><Link to={'/games/strategy'}>Strategy</Link></a>
+                <a className='item-link'><Link to={'/games/action-rpg'}>ActionRpg</Link></a>
+                <a className='item-link'><Link to={'/games'}>All Games </Link></a>
+              {/*   <a className='item-link'><Link to={'/games'}>Em breve !!</Link></a>
+                <a className='item-link'><Link to={'/games'}>Em breve !!</Link></a>
+                <a className='item-link'><Link to={'/games'}>Em breve !!</Link></a>
+ */}
             </ul>
         </div>
     )
